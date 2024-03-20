714c18b28f284570a3d1cab20a3f22bec162c98f
laurabpaulsen <202005791@post.au.dk>
2024-03-20 16:53:15 +0100

    rm scale, check input, add attribute x_img

--- timeseriesCNN/timeseries2img.py
+++ timeseriesCNN/timeseries2img.py
@@ -4,49 +4,55 @@ Functions for timeseries 2 img
 import numpy as np
 from pyts.image import GramianAngularField, MarkovTransitionField
 
+from utils import _check_valid_types
 
 class Data: 
-    def __init__(self, X, y, image_size):
+    def __init__(self, X:np.array, y:np.array):
         '''
         X.shape = (n_samples, n_timestamps) or (n_samples, n_timestamps, n_features)
         y.shape = (n_samples, )
         '''
         self.X = X
         self.y = y
-
-        if type(image_size) == float: # if image_size is a float, we calculate
-            self.image_size = int(X.shape[1] * image_size)
-        else:
-            self.image_size = image_size
+        self.X_img = None
 
         # check if X and y have the same number of samples
         if X.shape[0] != len(y): 
             raise ValueError('X and y should have the same number of samples')
 
-    def scale(): 
-        pass
+    def transform_to_img(self, methods:list|str=["GASF, GADF, MTF"], image_size:float|int=1.0): 
+        '''
+        Convert the timeseries to images using the methods specified in the methods list.
+        '''
+        # if methods is a string, convert it to a list
+        if type(methods) == str:
+            methods = [methods]
 
-    def convert_to_img(self, methods=["GASF, GADF, MTF"]): 
-        """
-        """
+        # check if the methods are valid
+        _check_valid_types(methods, ["GASF", "GADF", "MTF"])
+
+        # prepare image size (convert to int if it is a float)
+        if type(image_size) == float: # if image_size is a float, we calculate the image size
+            image_size = int(X.shape[1] * image_size)
 
         # prepare empty numpy array for the images
         n_samples = self.X.shape[0]
         n_methods = len(methods)
-        n_features = self.X.shape[2] if len(self.X.shape) == 3 else 1
+        n_features = self.X.shape[2] if len(self.X.shape) == 3 else 1    
 
-        im = np.zeros((n_samples, self.image_size, self.image_size, n_features, n_methods)) # img has to be symmetric
+        im = np.zeros((n_samples, image_size, image_size, n_features, n_methods)) # img has to be symmetric
 
-        for method_index, method in enumerate(methods):
+        for method_index, method in enumerate(methods): ## loop over all the methods
 
+            # make more elegant later!
             if type(method) == str:
                 if method == "GASF":
-                    transformer = GramianAngularField(image_size=self.image_size, method='summation')
+                    transformer = GramianAngularField(image_size=image_size, method='summation')
                 elif method == "GADF":
-                    transformer = GramianAngularField(image_size=self.image_size, method='difference')
+                    transformer = GramianAngularField(image_size=image_size, method='difference')
                 elif method == "MTF":
-                    transformer = MarkovTransitionField(image_size=self.image_size)
-            else:
+                    transformer = MarkovTransitionField(image_size=image_size)
+            else: # allows users to pass the transformers directly to include custom keyword arguments
                 transformer = method
 
             for feature in range(n_features):
@@ -55,9 +61,11 @@ class Data:
 
                 im[:, :, :, feature, method_index] = transformer.fit_transform(X_tmp)
 
-        return im
+        self.X_img = im
 
 if __name__ == "__main__":
+
+    # for testing (keep for now, but remove later)
     from pyts.datasets import load_gunpoint
     X, _, y, _ = load_gunpoint(return_X_y=True)   
 
@@ -66,9 +74,9 @@ if __name__ == "__main__":
     print(X.shape, y.shape, len(y))
 
     # data
-    data = Data(X, y, image_size=0.5)
+    data = Data(X, y)
 
-    gaf = data.convert_to_img(methods=["GASF", "GADF", "MTF"])
+    data.transform_to_img(methods=["GASF", "GADaF", "MTF"], image_size=0.5)
 
-    print(gaf.shape)
+    print(data.X_img.shape)
 
